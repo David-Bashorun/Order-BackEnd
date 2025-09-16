3e6bd2d737035432f513699153f21f3cb6c52a31
David-Bashorun <davidbashorun@gmail.com>
2025-09-16 13:42:17 +0100

    m

--- utils/nodemailerConfig.js
+++
@@ -1,9 +0,0 @@
-const transporter = nodemailer.createTransport({
-  host: "smtp.ethereal.email",
-  port: 587,
-  secure: false, // true for 465, false for other ports
-  auth: {
-    user: "maddison53@ethereal.email",
-    pass: "jn7jnAPss4f63QBp6D",
-  },
-});
